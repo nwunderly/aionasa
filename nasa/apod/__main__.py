f33d0a650a98e396eae48b1696f6ac15b5d7ac8d
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-07-08 15:02:52 -0400

    add batch request and upgrade cli tool
    nasa.APOD now supports date ranges with batch_get.
    
    APOD CLI tool now supports date ranges and can dump to json/yaml
    
    csv format is possible for the future but currently not working.

--- nasa/apod/__main__.py
+++ nasa/apod/__main__.py
@@ -3,6 +3,9 @@ import aiohttp
 import asyncio
 import argparse
 
+import json
+import yaml
+
 from datetime import date, timedelta, datetime
 
 
@@ -10,11 +13,17 @@ from datetime import date, timedelta, datetime
 TEST SCRIPT FOR APOD API WRAPPER
 """
 
+VALID_FILE_TYPES = ['csv', 'json', 'yaml']
+
+
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('day')
-parser.add_argument('--print', action='store_true')
+parser.add_argument('--date', '-d')
+parser.add_argument('--start-date', '-start')
+parser.add_argument('--end-date', '-end')
+parser.add_argument('--print', '-p', action='store_true')
+parser.add_argument('--dump', '-D')
 
 
 def process_date(day):
@@ -29,7 +38,26 @@ def process_date(day):
     return date(dt.year, dt.month, dt.day)
 
 
-async def main(day, _print):
+def dump_to_json(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+
+
+def dump_to_csv(data, filename):
+    with open(filename, 'w') as f:
+        if not isinstance(data, list):  # batch_get
+            data = [data]
+        for _dict in data:
+            items = [str(value) for value in _dict.values()]
+            f.write(','.join(items) + '\n')
+
+
+def dump_to_yaml(data, filename):
+    with open(filename, 'w') as f:
+        yaml.dump(data, f, yaml.Dumper)
+
+
+async def get(day, _print, dump):
     day = process_date(day)
 
     apod = APOD()
@@ -44,6 +72,71 @@ async def main(day, _print):
             s += f"{key}: {value}\n"
         print(s)
 
-if __name__ == "__main__":
+    if dump:
+        split = dump.split('.')
+        ext = split[-1]
+
+        if ext.lower() == 'json':
+            dump_to_json(response, dump)
+        elif ext.lower() == 'csv':
+            dump_to_csv(response, dump)
+        elif ext.lower() == 'yaml':
+            dump_to_yaml(response, dump)
+
+
+async def batch_get(start_date, end_date, _print, dump):
+    start_date = process_date(start_date)
+    end_date = process_date(end_date)
+
+    apod = APOD()
+
+    response = await apod.batch_get(start_date, end_date, as_json=True)
+
+    await apod.close()
+
+    if _print:
+        s = ""
+        for entry in response:
+            for key, value in entry.items():
+                s += f"{key}: {value}\n"
+            s += "\n"
+        print(s)
+
+    if dump:
+        split = dump.split('.')
+        ext = split[-1]
+
+        if ext.lower() == 'json':
+            dump_to_json(response, dump)
+        elif ext.lower() == 'csv':
+            dump_to_csv(response, dump)
+        elif ext.lower() in {'yaml', 'yml'}:
+            dump_to_yaml(response, dump)
+
+
+
+async def main():
     args = parser.parse_args()
-    asyncio.run(main(args.day, args.print))
+    _date = args.date
+    start_date = args.start_date
+    end_date = args.end_date
+    _print = args.print
+    dump = args.dump
+
+    if _date:
+        if start_date or end_date:
+            raise argparse.ArgumentError("date and start-date/end-date are not compatible arguments.")
+        await get(_date, _print, dump)
+
+    elif start_date or end_date:
+        if not (start_date and end_date):
+            raise argparse.ArgumentError("start-date and end-date are both required arguments when requesting a range.")
+        await batch_get(start_date, end_date, _print, dump)
+
+    else:
+        await get('today', _print, dump)
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- nasa/apod/apod_client.py
+++ nasa/apod/apod_client.py
@@ -11,7 +11,15 @@ ApodEntry = namedtuple('ApodEntry', ['date', 'copyright', 'title', 'explanation'
 class APOD:
     """
     Client for NASA Astronomy Picture of the Day.
+
+    Parameters for APOD API:
+        - date: The date of the APOD image to retrieve. Defaults to 'today'.
+        - start_date: The first date to return when requesting a list of dates.
+        - end_date: The last date to return when requesting a list of dates. Range is inclusive.
+        - hd: Bool indicating whether to retrieve the URL for the high resolution image. Defaults to 'False'.
+        - concept_tags: DISABLED FOR THIS ENDPOINT.
     """
+
     def __init__(self, api_key='DEMO_KEY', session=None):
         """
         Initializes the APOD class.
@@ -34,7 +42,7 @@ class APOD:
 
     async def get(self, date: datetime.date = None, hd: bool = None, as_json: bool = False):
         """
-        Performs a GET request to NASA's APOD API.
+        Retrieves a single item from NASA's APOD API.
 
         :param date: The date of the APOD image to retrieve. Defaults to 'today'.
         :param hd: Retrieve the URL for the high resolution image. Defaults to 'False'.
@@ -45,6 +53,9 @@ class APOD:
             raise TypeError("Argument 'date' must be an instance of 'datetime.date'.")
         if not (isinstance(hd, bool) or hd is None):
             raise TypeError("Argument 'hd' must be an instance of 'bool'.")
+        if not isinstance(as_json, bool):
+            raise TypeError("Argument 'as_json' must be an instance of 'bool'.")
+
 
         if date is None:  # parameter will be left out of the query.
             date = ''
@@ -75,6 +86,62 @@ class APOD:
                 service_version=json.get('service_version')
             )
             return entry
+
+    async def batch_get(self, start_date: datetime.date, end_date: datetime.date,
+                        hd: bool = None, as_json: bool = False):
+        """
+        Retrieves multiple items from NASA's APOD API. Returns a list of APOD entries.
+
+        :param start_date: The first date to return when requesting a list of dates.
+        :param end_date: The last date to return when requesting a list of dates. Range is inclusive.
+        :param hd: Retrieve the URL for the high resolution image. Defaults to 'False'.
+        :param as_json: Bool indicating whether to return a dict containing the raw returned json data instead of the normal named tuple.
+        :return: A list of named tuples containing data returned by the API.
+        """
+        if not isinstance(start_date, datetime.date):
+            raise TypeError("Argument 'start_date' must be an instance of 'datetime.date'.")
+        if not isinstance(start_date, datetime.date):
+            raise TypeError("Argument 'end_date' must be an instance of 'datetime.date'.")
+        if not (isinstance(hd, bool) or hd is None):
+            raise TypeError("Argument 'hd' must be an instance of 'bool'.")
+        if not isinstance(as_json, bool):
+            raise TypeError("Argument 'as_json' must be an instance of 'bool'.")
+
+
+        start_date = 'start_date=' + start_date.strftime('%Y-%m-%d') + '&'
+        end_date = 'end_date=' + end_date.strftime('%Y-%m-%d') + '&'
+
+        if hd is None:  # parameter will be left out of the query.
+            hd = ''
+        else:
+            hd = 'hd=' + str(hd) + '&'
+
+        request = f"https://api.nasa.gov/planetary/apod?{start_date}{end_date}{hd}api_key={self._api_key}"
+
+        async with self._session.get(request) as response:
+            json = await response.json()
+
+        if as_json:
+            return json
+
+        else:
+            result = []
+
+            for item in json:
+
+                entry = ApodEntry(
+                    date=item.get('date'),
+                    copyright=item.get('copyright'),
+                    title=item.get('title'),
+                    explanation=item.get('explanation'),
+                    url=item.get('url'),
+                    hdurl=item.get('hdurl'),
+                    media_type=item.get('media_type'),
+                    service_version=item.get('service_version')
+                )
+                result.append(entry)
+
+            return result
 
     async def close(self):
         if self._session:
