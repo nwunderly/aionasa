0ddd2891e9fb6ebd2cf62d7db4f65147986d5109
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-07-24 15:12:50 -0400

    beginning of exoplanet API

---
+++ aionasa/exoplanet/api.py
@@ -0,0 +1,30 @@
+
+import aiohttp
+import asyncio
+import datetime
+
+try:
+    import pandas
+except ImportError:
+    pandas = None
+
+from ..client import BaseClient
+from ..errors import *
+
+
+class Exoplanet(BaseClient):
+    """
+    Client for NASA Exoplanet Archive API.
+
+    In-depth documentation for this API can be found at https://exoplanetarchive.ipac.caltech.edu/docs/program_interfaces.html
+
+    Parameters for Exoplanet API:
+        -
+    """
+
+    async def raw_get(self, query):
+        pass
+
+    async def get(self, table: str, select=None, count=None, colset=None, where=None, order=None, ra=None, dec=None, format=None):
+        pass
+
---
+++ aionasa/exoplanet/query.py
@@ -0,0 +1,92 @@
+
+
+from ..errors import *
+
+
+class _QueryString:
+    def __init__(self, string):
+        self.str = string
+
+    def __bool__(self):
+        return NotImplemented
+
+    def __str__(self):
+        return self.str
+
+    def __repr__(self):
+        return f"QueryString('{self}')"
+
+    def __and__(self, other):
+        return _QueryString(f"{self.str} and {other.str}")
+
+    def __or__(self, other):
+        return _QueryString(f"{self.str} or {other.str}")
+
+
+class _Query:
+    def __init__(self, name):
+        self.name = name
+
+    def __bool__(self):
+        return NotImplemented
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"Query('{self}')"
+
+    def __eq__(self, other):
+        return _QueryString(f"{self.name}={other}")
+
+    def __gt__(self, other):
+        return _QueryString(f"{self.name}>{other}")
+
+    def __lt__(self, other):
+        return _QueryString(f"{self.name}<{other}")
+
+    def __ge__(self, other):
+        return _QueryString(f"{self.name}>={other}")
+
+    def __le__(self, other):
+        return _QueryString(f"{self.name}<={other}")
+
+    def like(self, item):
+        return _QueryString(f"{self.name} like {item}")
+
+    def __contains__(self, item):
+        return _QueryString(f"{self.name} like {item}")
+
+
+"""
+PUT QUERY OPTIONS HERE
+
+ex:
+    ra = _Query('ra')
+    dec = _Query('dec')
+    pl_discmethod = _Query('pl_discmethod')
+"""
+
+
+# =====================================================================================================================================================
+
+class _Format:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+csv = _Format('csv')
+bar = _Format('bar')
+bar_delimited = _Format('bar_delimited')
+pipe = _Format('pipe')
+pipe_delimited = _Format('pipe_delimited')
+xml = _Format('xml')
+vo_table = _Format('vo_table')
+json = _Format('json')
+
+
+
+
