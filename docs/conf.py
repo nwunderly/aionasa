d9ac1d0a329fd0be6b302e03158595113df82aac
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-10-08 23:49:41 -0400

    possibly fix readthedocs/aiohttp issue

--- docs/conf.py
+++ docs/conf.py
@@ -59,3 +59,10 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# intersphinx_mapping = {
+#   'py': ('https://docs.python.org/3', None),
+#   'aio': ('https://docs.aiohttp.org/en/stable/', None),
+# }
+
+autodoc_mock_imports = ['aiohttp']
