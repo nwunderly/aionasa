ff58891add878e6718b665d153fd2b71031efe33
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-11-01 17:50:25 -0500

    Update data.py

--- aionasa/epic/data.py
+++ aionasa/epic/data.py
@@ -8,6 +8,12 @@ class EarthImage:
     """A NASA EPIC image asset.
 
     """
-    def __init__(self, client, url):
+    def __init__(self, client, url, **kwargs):
         self.client = client
         self.url = url
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+
+
