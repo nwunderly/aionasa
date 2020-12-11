add2b9da5451abe55028ea21697fe99bb9da73b1
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-12-10 19:55:00 -0500

    documentation for epic/__main__.py and utils.py

--- aionasa/epic/__main__.py
+++ aionasa/epic/__main__.py
@@ -6,24 +6,27 @@ from .api import EPIC
 from .img_viewer import open_gui
 from ..utils import date_strptime
 
-"""
-USAGE:
-    [arg] - optional
-    (arg) - required
 
-python -m aionasa.epic [--date DATE] [--collection COLLECTION] (IMG_FOLDER)
-    DATE:           YYYY-MM-DD
-    COLLECTION:     natural | enhanced | natural,enhanced
-    IMG_FOLDER:     (directory to download images to)
-"""
+__doc__ = "Download some images from NASA's EPIC archive and open them in a gui browser."
+usage = "python -m aionasa.epic [-h] [--date DATE] [--collection COLLECTION] img_folder"
 
 
 def argument_parser():
-    parser = argparse.ArgumentParser()
+    """Generates the parser used by the aionasa.epic.__main__ script."""
+    parser = argparse.ArgumentParser(description=__doc__, usage=usage)
 
-    parser.add_argument('--date', '-d', type=date_strptime, default=None)
-    parser.add_argument('--collection', '-c', default='natural')
-    parser.add_argument('img_folder')
+    parser.add_argument(
+        '--date', '-d', type=date_strptime, default=None,
+        help="Format: YYYY-MM-DD"
+    )
+    parser.add_argument(
+        '--collection', '-c', default='natural',
+        help="Collection to get images from. Should be 'natural', 'enhanced', or 'natural,enhanced'"
+    )
+    parser.add_argument(
+        'img_folder',
+        help='Directory to download the images to.'
+    )
 
     return parser
 
@@ -37,6 +40,7 @@ async def _task(coro, arg):
 
 
 async def setup(date, path, collection):
+    """Downloads all EPIC images in a collection to a directory given by the 'path' parameter."""
     # make image directory if necessary
     if not os.path.exists(path):
         os.mkdir(path)
@@ -51,14 +55,15 @@ async def setup(date, path, collection):
 
         # download the images asynchronously
         print('downloading', len(images), 'images.')
-        await asyncio.gather(*[_task(image.save, path + '/' + image.filename) for image in images])
+        tasks = [_task(image.save, path + '/' + image.filename) for image in images]
+        await asyncio.gather(*tasks)
 
 
 async def main():
-    args = argument_parser().parse_args()
     await setup(args.date, args.img_folder, args.collection.split(','))
     open_gui(args.img_folder)
 
 
 if __name__ == '__main__':
+    args = argument_parser().parse_args()
     asyncio.run(main())
--- aionasa/utils.py
+++ aionasa/utils.py
@@ -1,12 +1,25 @@
 from datetime import datetime, date, timedelta
 
 
-def date_strptime(day):
+def date_strptime(date_string):
+    """Converts a YYYY-MM-DD string into a datetime.date object.
+
+    Equivalent to strptime with format string "%Y-%m-%d".
+    Also accepts 'today' and 'yesterday' as shortcuts.
+
+    Parameters
+    ----------
+    date_string: String to format.
+
+    Returns
+    -------
+    datetime.date: The converted date.
+    """
     keywords = {
         'today': date.today(),
         'yesterday': date.today() - timedelta(days=1)
     }
-    if day in keywords.keys():
-        return keywords[day]
+    if date_string in keywords.keys():
+        return keywords[date_string]
 
-    return datetime.strptime(day, f'%Y-%m-%d').date()
+    return datetime.strptime(date_string, f'%Y-%m-%d').date()
