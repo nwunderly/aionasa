723aa55e93e029d65463c5739761da1e97cf012a
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-12-23 20:30:24 -0500

    test different format for docs
    testing docstrings without `:class:`

--- aionasa/apod/api.py
+++ aionasa/apod/api.py
@@ -27,11 +27,11 @@ class APOD(BaseClient):
 
     Parameters
     ----------
-    api_key: :class:`str`
+    api_key: `str`
         NASA API key to be used by the client.
-    session: Optional[:class:`aiohttp.ClientSession`]
+    session: `Optional[aiohttp.ClientSession]`
         Optional ClientSession to be used for requests made by this client. Creates a new session by default.
-    rate_limiter: Optional[:class:`RateLimiter`]
+    rate_limiter: Optional[`RateLimiter`]
         Optional RateLimiter class to be used by this client. Uses the library's internal global rate limiting by default.
     """
 
@@ -47,16 +47,16 @@ class APOD(BaseClient):
 
         Parameters
         ----------
-        date: :class:`datetime.Date`
+        date: `datetime.Date`
             The date of the APOD image to retrieve. Defaults to ``'today'``.
-        hd: :class:`bool`
+        hd: `bool`
             Bool indicating whether to retrieve the URL for the high resolution image. Defaults to ``False``.
-        as_json: :class:`bool`
+        as_json: `bool`
             Bool indicating whether to return the raw returned json data instead of the normal AstronomyPicture object. Defaults to ``False``.
 
         Returns
         -------
-        :class:`AstronomyPicture`
+     `AstronomyPicture`
             An AstronomyPicture containing data returned by the API.
         """
 
@@ -110,18 +110,18 @@ class APOD(BaseClient):
 
         Parameters
         ----------
-        start_date: :class:`datetime.Date`
+        start_date: `datetime.Date`
             The first date to return when requesting a range of dates.
-        end_date: :class:`datetime.Date`
+        end_date: `datetime.Date`
             The last date to return when requesting a range of dates. Range is inclusive.
-        hd: :class:`bool`
+        hd: `bool`
             Bool indicating whether to retrieve the URL for the high resolution image. Defaults to ``False``.
-        as_json: :class:`bool`
+        as_json: `bool`
             Bool indicating whether to return a list of dicts containing the raw returned json data instead of the normal ``List[AstronomyPicture]``. Defaults to ``False``.
 
         Returns
         -------
-        List[:class:`AstronomyPicture`]
+        List `AstronomyPicture`]
             A list of AstronomyPicture objects containing data returned by the API.
         """
 
